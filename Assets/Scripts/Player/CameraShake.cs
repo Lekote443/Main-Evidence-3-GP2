d6fc37cb127ccd3ee779b78ef6028007195c7a65
Nehemiah <nmzileni04@gmail.com>
2026-06-17 20:16:10 +0200

    CameraShake

---
+++ Assets/Scripts/Player/CameraShake.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.Collections;
+
+public class CameraShake : MonoBehaviour
+{
+    private Vector3 originalPosition;
+
+    void Awake()
+    {
+        originalPosition = transform.localPosition;
+    }
+
+    public void Shake(float duration, float magnitude)
+    {
+        StartCoroutine(ShakeCoroutine(duration, magnitude));
+    }
+
+    IEnumerator ShakeCoroutine(float duration, float magnitude)
+    {
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+            transform.localPosition = originalPosition + new Vector3(x, y, 0);
+
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.localPosition = originalPosition;
+    }
+}
